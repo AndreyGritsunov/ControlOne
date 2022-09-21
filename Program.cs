21e1a71bd5ecb2ec240c5a0434c64f760716ef90
unknown <gritsunov85@gmail.com>
2022-09-21 13:45:28 +0600

    cycle

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿	
 string[] arrayNew = {"666", "123", "3445", "Вася", "245345455", "54545"};
 
+var listArray = new List<string>();
+
+for(int i = 0; i <  arrayNew.Length; i++){
+    if(arrayNew[i].Length <= 3) listArray.Add(arrayNew[i]);   
+}
