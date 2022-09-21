7567c787d8c3ff450e383c757f05fd0d1124ec3c
unknown <gritsunov85@gmail.com>
2022-09-21 13:58:50 +0600

    terms

--- Program.cs
+++ Program.cs
@@ -6,3 +6,11 @@ var listArray = new List<string>();
 for(int i = 0; i <  arrayNew.Length; i++){
     if(arrayNew[i].Length <= 3) listArray.Add(arrayNew[i]);   
 }
+
+if(listArray.Count > 0){
+    string[] array = new string[listArray.Count];
+    listArray.CopyTo(array);
+}
+else{
+    Console.Write("Нет строк с количеством меньше или равно 3-м");
+}
